b9f3b6570142b7ff7ed57c0338d5f94f75014efe
HuyVN <buiiduchuy@gmail.com>
2024-05-16 14:28:40 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+let arrInput = document.querySelectorAll("input");
+//enforce that only a float can be inputed
+function enforceFloat() {
+  var valid = /^\-?\d+\.\d*$|^\-?[\d]*$/;
+  var number = /\-\d+\.\d*|\-[\d]*|[\d]+\.[\d]*|[\d]+/;
+  if (!valid.test(this.value)) {
+    var n = this.value.match(number);
+    // this.value = n ? n[0] : "";
+    if ((this.value = n)) {
+      n[0];
+    } else {
+      ("");
+      // this.setCustomValidity("Nhập số nha bạn ơiiiiiii !");
+      // this.reportValidity();
+    }
+  }
+}
+arrInput.forEach((input) => {
+  input.onkeyup = input.onchange = enforceFloat;
+});
+
 // Bài tập 1
 let tinhTienLuong = document.getElementById("tinhTienLuong");
 tinhTienLuong.addEventListener("click", function () {
-  let soNgayLam = document.getElementById("soNgayLam").value;
-  let tongTienLuong = soNgayLam * 100000;
-  let ketQua = document.querySelector(".ketQuaBai1");
-  ketQua.innerHTML = `Số ngày làm việc là ${soNgayLam} , Tổng tiền lương tháng này là ${tongTienLuong.toLocaleString(
-    "it-IT",
-    { style: "currency", currency: "VND" }
-  )}`;
+  let soNgayLam = Number(document.getElementById("soNgayLam").value);
+  if (soNgayLam == "") {
+    return false;
+  } else {
+    let tongTienLuong = soNgayLam * 100000;
+    let ketQua = document.querySelector(".ketQuaBai1");
+    ketQua.innerHTML = `Số ngày làm việc là ${soNgayLam} , Tổng tiền lương tháng này là ${tongTienLuong.toLocaleString(
+      "it-IT",
+      { style: "currency", currency: "VND" }
+    )}`;
+  }
 });
 
 // Bài tập 2
@@ -18,9 +43,20 @@ tinhTrungBinh.addEventListener("click", function () {
   let number3 = Number(document.getElementById("number3").value);
   let number4 = Number(document.getElementById("number4").value);
   let number5 = Number(document.getElementById("number5").value);
-  let giaTriTB = (number1 + number2 + number3 + number4 + number5) / 5;
-  let ketQua = document.querySelector(".ketQuaBai2");
-  ketQua.innerHTML = `Các số bạn đã nhập là: ${number1} , ${number2} , ${number3} , ${number4} , ${number5} và giá trị trung bình của 5 số này là : ${giaTriTB}`;
+
+  if (
+    number1 == "" &&
+    number2 == "" &&
+    number3 == "" &&
+    number4 == "" &&
+    number5 == ""
+  ) {
+    return false;
+  } else {
+    let giaTriTB = (number1 + number2 + number3 + number4 + number5) / 5;
+    let ketQua = document.querySelector(".ketQuaBai2");
+    ketQua.innerHTML = `Các số bạn đã nhập là: ${number1} , ${number2} , ${number3} , ${number4} , ${number5} và giá trị trung bình của 5 số này là : ${giaTriTB}`;
+  }
 });
 
 // Bài tập 3
@@ -29,10 +65,14 @@ btnQuyDoi.addEventListener("click", function () {
   let soTienUSD = Number(document.getElementById("soTienUSD").value);
   let giaTriQuyDoi = soTienUSD * 23500;
   let ketquaBai3 = document.querySelector(".ketquaBai3");
-  ketquaBai3.innerHTML = `Giá trị tiền Việt là : ${giaTriQuyDoi.toLocaleString(
-    "it-IT",
-    { style: "currency", currency: "VND" }
-  )}`;
+  if (soTienUSD == "") {
+    return false;
+  } else {
+    ketquaBai3.innerHTML = `Giá trị tiền Việt là : ${giaTriQuyDoi.toLocaleString(
+      "it-IT",
+      { style: "currency", currency: "VND" }
+    )}`;
+  }
 });
 
 // Bài tập 4
@@ -43,8 +83,12 @@ btnTinh.addEventListener("click", function (e) {
   let dienTich = chieuDai * chieuRong;
   let chuVi = (chieuDai + chieuRong) * 2;
   let ketquaBai4 = document.querySelector(".ketquaBai4");
-  if (!chieuDai == "" && !chieuRong == "") {
-    ketquaBai4.innerHTML = `Chiều dài là ${chieuDai} , chiều rộng là ${chieuRong} ... Diện tích HCN là ${dienTich} , Chu vi HCN là ${chuVi}`;
+  if (chieuDai == "" || chieuRong == "") {
+    return false;
+  } else {
+    if (!chieuDai == "" && !chieuRong == "") {
+      ketquaBai4.innerHTML = `Chiều dài là ${chieuDai} , chiều rộng là ${chieuRong} ... Diện tích HCN là ${dienTich} , Chu vi HCN là ${chuVi}`;
+    }
   }
 });
 
@@ -55,7 +99,11 @@ btntinhTong.addEventListener("click", function () {
   let soHangChuc = parseInt(soNhapVao / 10);
   let soHangDonVi = soNhapVao % 10;
   let ketquaBai5 = document.querySelector(".ketquaBai5");
-  ketquaBai5.innerHTML = `Số đã nhập vào là ${soNhapVao} , tổng 2 ký số là ${
-    soHangChuc + soHangDonVi
-  }`;
+  if (soNhapVao == "") {
+    return false;
+  } else {
+    ketquaBai5.innerHTML = `Số đã nhập vào là ${soNhapVao} , tổng 2 ký số là ${
+      soHangChuc + soHangDonVi
+    }`;
+  }
 });
