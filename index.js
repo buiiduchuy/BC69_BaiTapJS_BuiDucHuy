944533282f8de9de5936a11ae948750c40c1c1c4
HuyVN <buiiduchuy@gmail.com>
2024-05-18 12:24:43 +0700

    modify

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ arrInput.forEach((input) => {
   input.onkeyup = input.onchange = enforceFloat;
 });
 
+let domID = function (id) {
+  return document.getElementById(id);
+};
+
 // Bài tập 1
-let tinhTienLuong = document.getElementById("tinhTienLuong");
+let tinhTienLuong = domID("tinhTienLuong");
 tinhTienLuong.addEventListener("click", function () {
-  let soNgayLam = Number(document.getElementById("soNgayLam").value);
+  let soNgayLam = Number(domID("soNgayLam").value);
   if (soNgayLam == "") {
     return false;
   } else {
@@ -38,11 +42,11 @@ tinhTienLuong.addEventListener("click", function () {
 // Bài tập 2
 let tinhTrungBinh = document.getElementById("tinhTrungBinh");
 tinhTrungBinh.addEventListener("click", function () {
-  let number1 = Number(document.getElementById("number1").value);
-  let number2 = Number(document.getElementById("number2").value);
-  let number3 = Number(document.getElementById("number3").value);
-  let number4 = Number(document.getElementById("number4").value);
-  let number5 = Number(document.getElementById("number5").value);
+  let number1 = Number(domID("number1").value);
+  let number2 = Number(domID("number2").value);
+  let number3 = Number(domID("number3").value);
+  let number4 = Number(domID("number4").value);
+  let number5 = Number(domID("number5").value);
 
   if (
     number1 == "" &&
@@ -60,9 +64,9 @@ tinhTrungBinh.addEventListener("click", function () {
 });
 
 // Bài tập 3
-let btnQuyDoi = document.getElementById("btnQuyDoi");
+let btnQuyDoi = domID("btnQuyDoi");
 btnQuyDoi.addEventListener("click", function () {
-  let soTienUSD = Number(document.getElementById("soTienUSD").value);
+  let soTienUSD = Number(domID("soTienUSD").value);
   let giaTriQuyDoi = soTienUSD * 23500;
   let ketquaBai3 = document.querySelector(".ketquaBai3");
   if (soTienUSD == "") {
@@ -76,10 +80,10 @@ btnQuyDoi.addEventListener("click", function () {
 });
 
 // Bài tập 4
-let btnTinh = document.getElementById("btnTinh");
+let btnTinh = domID("btnTinh");
 btnTinh.addEventListener("click", function (e) {
-  let chieuDai = Number(document.getElementById("chieuDai").value);
-  let chieuRong = Number(document.getElementById("chieuRong").value);
+  let chieuDai = Number(domID("chieuDai").value);
+  let chieuRong = Number(domID("chieuRong").value);
   let dienTich = chieuDai * chieuRong;
   let chuVi = (chieuDai + chieuRong) * 2;
   let ketquaBai4 = document.querySelector(".ketquaBai4");
@@ -93,9 +97,9 @@ btnTinh.addEventListener("click", function (e) {
 });
 
 // Bài tập 5
-let btntinhTong = document.getElementById("btntinhTong");
+let btntinhTong = domID("btntinhTong");
 btntinhTong.addEventListener("click", function () {
-  let soNhapVao = Number(document.getElementById("nhapSo").value);
+  let soNhapVao = Number(domID("nhapSo").value);
   let soHangChuc = parseInt(soNhapVao / 10);
   let soHangDonVi = soNhapVao % 10;
   let ketquaBai5 = document.querySelector(".ketquaBai5");
